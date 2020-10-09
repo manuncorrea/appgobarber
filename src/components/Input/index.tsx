22f328790c513d632246d57aa78ebe345ee8f1c9
Emanuele Correa <manuncorrea@hotmail.com>
2020-10-09 07:15:55 -0300

    criando components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput } from './styles';
+
+interface InputProps extends TextInputProps {
+    name: string;
+    icon: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest}) => (
+    <Container>
+        <TextInput {...rest} />
+    </Container>
+);
+
+export default Input;
