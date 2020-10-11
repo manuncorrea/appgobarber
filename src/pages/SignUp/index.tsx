af512c137497ec3b41a43bd054d37b35bd32435e
Emanuele Correa <manuncorrea@hotmail.com>
2020-10-10 22:19:28 -0300

    tela de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSign, BackToSignText } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
-    return <Container />
-}
+    const navigation = useNavigation();
+
+    return (
+        <>
+            <KeyboardAvoidingView 
+                 style={{ flex: 1 }}
+                 behavior={ Platform.OS === 'ios' ? 'padding' : undefined}
+                 enabled
+            >
+                <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+                    <Container >
+                        <Image source={logoImg} />
+
+                        <View>
+                        <Title>Crie sua conta</Title>
+                        </View>
+
+                        <Input name="name" icon="user" placeholder="Nome" />
+                        <Input name="email" icon="mail" placeholder="E-mail" />
+                        <Input name="password" icon="lock" placeholder="Senha" />
+
+                        <Button onPress={() =>{
+                            console.log('Deu');
+                        }}>Entrar</Button>
+
+                    </Container>
+                 </ScrollView>
+
+                <BackToSign onPress={() => navigation.goBack()}>
+                    <Icon name="arrow-left" size={20} color="#fff"/>
+                    <BackToSignText>Voltar para logon</BackToSignText>
+                </BackToSign>
+            </KeyboardAvoidingView>
+        </>
+        );
+    
+};
 
 
 export default SignUp;
