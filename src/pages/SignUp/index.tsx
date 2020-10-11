94fc471f1ba1290291ab82ab212d32cd28777549
Emanuele Correa <manuncorrea@hotmail.com>
2020-10-11 08:39:47 -0300

    integrando o unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,6 +14,7 @@ import { Container, Title, BackToSign, BackToSignText } from './styles';
 import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
 
     return (
@@ -29,13 +32,15 @@ const SignUp: React.FC = () => {
                         <Title>Crie sua conta</Title>
                         </View>
 
-                        <Input name="name" icon="user" placeholder="Nome" />
-                        <Input name="email" icon="mail" placeholder="E-mail" />
-                        <Input name="password" icon="lock" placeholder="Senha" />
+                        <Form ref={formRef} onSubmit={(data) => { console.log(data)}}>
+                            <Input name="name" icon="user" placeholder="Nome" />
+                            <Input name="email" icon="mail" placeholder="E-mail" />
+                            <Input name="password" icon="lock" placeholder="Senha" />
 
-                        <Button onPress={() =>{
-                            console.log('Deu');
-                        }}>Entrar</Button>
+                            <Button onPress={() => formRef.current?.submitForm()}>
+                                Entrar
+                            </Button>
+                        </Form>
 
                     </Container>
                  </ScrollView>
